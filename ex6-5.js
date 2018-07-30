40a33403551b779fa18c169b135c098f4ec1f605
zm17086 <zm17086@josai.ac.jp>
2018-07-29 20:59:55 -0400

    message

---
+++ ex6-5.js
@@ -0,0 +1,3 @@
+function syahello(){var who='world';
+alert('Hello,'+who+'!');}
+function taro(){who='太郎';}function hanako(){who='花子';}
---
+++ myjs.js
@@ -0,0 +1,8 @@
+//alert('Hello,youichi!');
+//alert(1+1);
+alert(Math.sqrt(1+1));
+2+3
+'2'+'3'
+Numver('2')+Number('3')
+'2'+3
+Number('2')+3
